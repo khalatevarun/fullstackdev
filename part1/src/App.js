3dcd4f94db7c9e4615798033cd1e4bbb278fc79c
Varun Khalate <khalatevarun@gmail.com>
2021-04-08 13:43:12 +0530

    Modify the variable definitions to use objects

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = (props) => {
   return(
     <h1>
-    {props.course}
+    {props.course.name}
   </h1>
   )
 
@@ -13,35 +13,51 @@ const Header = (props) => {
 
 const Content = (props) => {
   return(
+    <>
       <p>
-        {props.part} {props.exercise}
+       {props.course.parts[0].name} {props.course.parts[0].exercises}
+      </p>
+      <p>
+      {props.course.parts[1].name} {props.course.parts[1].exercises}
+      </p>
+      <p>
+      {props.course.parts[2].name} {props.course.parts[2].exercises}
       </p>  
+      </>
   )
 }
 
 const Total = (props) => {
   return(
     <p>
-      Number of exercises {props.total}
+      Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises+props.course.parts[2].exercises }
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return(
     <div>
      <Header course = {course}/>
-     <Content part = {part1} exercise = {exercises1} />
-     <Content part = {part2} exercise = {exercises2} />
-     <Content part = {part3} exercise = {exercises3} />
-     <Total total = {exercises1+exercises2+exercises3}/>
+     <Content course = {course} />
+     <Total  course = {course}/>
     </div>
   )
 }
