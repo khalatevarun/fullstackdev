b8811b420f4e37cefa5de36b6b9f192ea8555c0e
Varun Khalate <khalatevarun@gmail.com>
2021-04-13 20:19:33 +0530

    Add a person to phonebook - step 1

---
+++ part2_phonebook/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map((person, i) => 
+          <p key={i}>
+            {person.name}
+          </p>
+  )}
+
+    </div>
+  )
+}
+
+export default App
